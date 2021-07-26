c0c55a15ad2e129e09dcd3a0b05c4ec248f57a37
Asa Riley <asa.riley@gmail.com>
2021-07-26 17:40:14 -0400

    Initial Commit

---
+++ Shared/swim_ontarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  swim_ontarioApp.swift
+//  Shared
+//
+//  Created by Asa Riley on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct swim_ontarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
